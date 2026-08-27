990ab31ed0e24a1bd43eb68f29022a5e5c220851
Mateus B. Reck <105645548+Mtreck@users.noreply.github.com>
2026-08-27 11:21:25 +0000

    Fim

--- 2ºDS - Desenvolvimento - Front- end/experimento06.js
+++
@@ -1,6 +0,0 @@
-const botaoAbrir = document.getElementById("btnAbrirModal");
-const modalContainer = document.getElementById("modalContainer");
-
-botaoAbrir.addEventListener("click", function(){
-    
-})
